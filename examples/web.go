108a258b58ab7a5a55b4f6d9a12b74fe4a90ee62
Francesc Campoy <campoy@golang.org>
2019-02-13 21:16:12 +0100

    web server example

---
+++ examples/web.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, web!")
+}
