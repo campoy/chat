9bb3b2e8ed11358d6025172351931a54a510b260
Francesc Campoy <campoy@golang.org>
2019-02-13 20:58:42 +0100

    Hello, net

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, CERN!")
+	l, err := net.Listen("tcp", ":4000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintln(conn, "Hello, net")
+		conn.Close()
+	}
 }
